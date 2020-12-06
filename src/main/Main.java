d8b1769c0df703f8c780c25ec6ee75825d3955ca
FAITH <Dechapon.n@hotmail.com>
2020-12-06 13:06:28 +0700

    update project class diagram

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,64 +1,20 @@
 package main;
 
-import com.formdev.flatlaf.intellijthemes.*;
 import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneDarkContrastIJTheme;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneLightContrastIJTheme;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatLightOwlContrastIJTheme;
 import controller.LoginController;
-import controller.RegisterController;
-import model.*;
-import java.sql.*;
 import javax.swing.*;
-import mysql.*;
-
-import views.RegisterGUI;
 
 public class Main {
     public static void main(String[] args) {
-        //testGUI();
-        //test();
-        testLoginGUI();
-        //testRegisterGUI();
-
+        startProgram();
     }
-    public static void test(){
-        try{
-            MySQLConnector mysql = new MySQLConnector("MyDatabase", "root", "031961698");
-            //User kuygen = new User(mysql, "kuygame", "123456", "GameKuy", "Dog", "Dog@dog.com", "0987654321");
-            User kuygen = new User(mysql, "kuygame", "123456");
-            //kuygen.deleteAccount();
-            if(kuygen.getAccount()){
-                Item testitem = new Item(kuygen.getId(), "Banana", "fruit", 12.5, 0.5, 1);
-                String idInformation = String.format("%d %s %s %s %s %s %s", kuygen.getId(),
-                        kuygen.getUsername(), kuygen.getPassword(), kuygen.getFirstname(), kuygen.getLastname(),
-                kuygen.getEmail(), kuygen.getTel());
-                String itemInformation = String.format("%d %s %s %f %f %d", testitem.getUser_id(), testitem.getItem_name(),
-                testitem.getItem_type(), testitem.getItem_price(), testitem.getItem_weight(), testitem.getQuantity());
-                System.out.println(idInformation);
-                System.out.println(itemInformation);
-                System.out.println(kuygen.addItem(testitem));
-                //System.out.println(kuygen.deleteItem(testitem));
-                //System.out.println(kuygen.changePassword("123456"));
-            }
-            else{
-                System.out.println("Failed!!");
-            }
-        }
-        catch(SQLException e){
-            System.out.println(e);
-        }
-    }
-
-    public static void testLoginGUI(){
+    public static void startProgram(){
         try{
             UIManager.setLookAndFeel(new FlatAtomOneDarkContrastIJTheme());
-            //UIManager.setLookAndFeel(new FlatAtomOneLightContrastIJTheme());
-            LoginController lc = new LoginController();
-            lc.getUsernameField().setText("kuygame");
-            lc.getPasswordField().setText("12345678");
+            new LoginController();
         }
-        catch(Exception e){
-            System.out.println(e);
+        catch(UnsupportedLookAndFeelException e){
+            e.printStackTrace();
         }
     }
 }
